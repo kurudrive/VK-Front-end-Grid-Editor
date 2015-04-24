d142d942b6d10714239a7d761c24a98e6dd384ea
kurudrive <kurudrive@gmail.com>
2015-04-24 18:46:59 +0900

    保存ボタンを本文下部にも表示されるように変更

--- js/vkEdit2_main.js
+++ js/vkEdit2_main.js
@@ -473,7 +473,7 @@ function vkEdit_btn_delCol(){
 function vkEdit_display_masterPanel(){
 	var html_masterPanel = '<div id="vkEdit_masterCtrlPanel" class="vkEdit_masterCtrlPanel vkEdit_masterCtrlPanel_alert row"><div class="col-md-2"><button id="submit" class="button button-primary button-large"><i class="fa fa-floppy-o"></i> Save</button></div><div class="col-md-10"><p>Change has not yet been saved.</p></div></div>';
 	if( ! jQuery('#vkEdit_editWrap').prev().hasClass('vkEdit_masterCtrlPanel')){
-		jQuery('#vkEdit_editWrap').before(html_masterPanel);
+		jQuery('#vkEdit_editWrap').before(html_masterPanel).after(html_masterPanel)
 		/*-------------------------------------------*/
 		// 保存処理（php側に記載）
 		/*-------------------------------------------*/
